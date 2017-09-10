739615650eb04de12ce3fe58c9989f3828bbc678
jwwitherspoon <jwwitherspoon@gmail.com>
2017-09-10 15:15:13 -0400

    Update SandwichDecorator.java

--- SandwichDecorator.java
+++ SandwichDecorator.java
@@ -1,8 +1,18 @@
-
+//Base class for all decorators
 public class SandwichDecorator extends Sandwich {
+	//Sandwich field
+	protected Sandwich sandwich;
 	
+	//Getter and setter for sandwich
+	public Sandwich getSandwich() {
+		return this.sandwich;
+	}
+	public void setSandwich(Sandwich sandwich) {
+		this.sandwich = sandwich;
+	}
+	
+	@Override
 	public double cost() {
 		return 0;
 	}
-	
 }
