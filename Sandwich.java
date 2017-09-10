1bf3ace24129b85b2d7f48aa62159ac8ca1de130
jwwitherspoon <jwwitherspoon@gmail.com>
2017-09-10 15:14:54 -0400

    Update Sandwich.java

--- Sandwich.java
+++ Sandwich.java
@@ -1,10 +1,21 @@
-
+//Base class for all sandwiches
 public abstract class Sandwich {
-	String description;
+	//Description field
+	protected String description = "I am a sandwich.";
 	
-	public void getDescription() {
+	//Getter and setter for description
+	public String getDescription() {
+		return this.description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	//Prints the description of the sandwich
+	public void printDescription() {
 		System.out.println(description);
 	}
 	
+	//Abstract method for computing cost of sandwich
 	public abstract double cost();
 }
