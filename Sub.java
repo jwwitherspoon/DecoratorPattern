f0240715891d620fdce45ecd5b60e99e999f1de4
jwwitherspoon <jwwitherspoon@gmail.com>
2017-09-10 15:15:36 -0400

    Update Sub.java

--- Sub.java
+++ Sub.java
@@ -1,10 +1,11 @@
-
+//Sub sandwich
 public class Sub extends Sandwich {
-	
+	//Add description of Sub to description of sandwich
 	public Sub() {
-		description = "I am a sub sandwich. I am really long.";
+		description = description.concat("\nI am a sub sandwich. I am really long.");
 	}
 	
+	@Override
 	public double cost() {
 		return 5.00;
 	}
