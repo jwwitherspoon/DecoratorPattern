8812ab1cfb950ba0efef39a2b6cca4ab1194f601
jwwitherspoon <jwwitherspoon@gmail.com>
2017-09-10 15:14:30 -0400

    Update Regular.java

--- Regular.java
+++ Regular.java
@@ -1,10 +1,11 @@
-
+//Regular sandwich
 public class Regular extends Sandwich {
-	
+	//Add description of Regular to description of sandwich
 	public Regular() {
-		description = "I am a regular sandwich made with two regular slices of bread.";
+		description = description.concat("\nI am a regular sandwich made with two regular slices of bread.");
 	}
 	
+	@Override
 	public double cost() {
 		return 3.00;
 	}
